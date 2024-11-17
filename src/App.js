7b56ce39df6b51eb395ee68ff350c86516703c82
Emmanuel Okeowo <okeowoemmanuelm@gmail.com>
2024-11-17 20:39:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function TipCalculator() {
+  const [bill, setBill] = useState(0);
+  const [yourRating, setYourRating] = useState(0);
+  const [friendRating, setFriendRating] = useState(0);
+
+  const calculateTip = () => {
+    const averageTipPercentage = (yourRating + friendRating) / 2;
+    return (bill * averageTipPercentage) / 100;
+  };
+
+  const reset = () => {
+    setBill(0);
+    setYourRating(0);
+    setFriendRating(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <div>
+          <label>How much was the bill?</label>
+          <input
+              type="number"
+              value={bill}
+              onChange={(e) => setBill(Number(e.target.value))}
+          />
+        </div>
+        <div>
+          <label>How did you like the service?</label>
+          <select
+              value={yourRating}
+              onChange={(e) => setYourRating(Number(e.target.value))}
+          >
+            <option value={0}>Dissatisfied (0%)</option>
+            <option value={5}>It was okay (5%)</option>
+            <option value={10}>It was good (10%)</option>
+            <option value={15}>Absolutely (20%)</option>
+          </select>
+        </div>
+        <div>
+          <label>How did your friend like the service?</label>
+            <select
+                value={friendRating}
+                onChange={(e) => setFriendRating(Number(e.target.value))}
+            >
+                <option value={0}>Dissatisfied (0%)</option>
+                <option value={5}>It was okay (5%)</option>
+                <option value={10}>It was good (10%)</option>
+                <option value={15}>Absolutely amazing! (15%)</option>
+            </select>
+        </div>
+          <h3>
+              You pay ${(bill + calculateTip()).toFixed(2)} (${bill.toFixed(2)} + ${calculateTip().toFixed(2)} tip)
+          </h3>
+          <button onClick={reset}>Reset</button>
+      </div>
   );
 }
 
-export default App;
+export default TipCalculator;
